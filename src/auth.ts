7703c73130c67183c3696656958f6cf9a4a07dfb
Hengleap EAR <hengleap70@gmail.com>
2026-03-15 22:09:54 +0700

    fix: import notifications table in src/auth.ts to solve build error

--- src/auth.ts
+++ src/auth.ts
@@ -2,7 +2,7 @@ import NextAuth from "next-auth";
 import { DrizzleAdapter } from "@auth/drizzle-adapter";
 import { db } from "@/db";
 import authConfig from "./auth.config";
-import { accounts, sessions, users, verificationTokens } from "@/db/schema";
+import { accounts, sessions, users, verificationTokens, notifications } from "@/db/schema";
 import { eq } from "drizzle-orm";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
